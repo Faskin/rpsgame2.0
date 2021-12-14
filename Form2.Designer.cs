9a77e2cda55d06db22c2751640501b202b439a75
opilane <opilane@HP-CZC6297BBB>
2021-12-14 14:32:39 +0200

    Add project files.

---
+++ Form2.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace rpsgame2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(694, 406);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::rpsgame2.Properties.Resources.quest;
+            this.pictureBox1.Location = new System.Drawing.Point(333, 276);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(124, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::rpsgame2.Properties.Resources.quest;
+            this.pictureBox2.Location = new System.Drawing.Point(333, 1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(124, 120);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label1.Location = new System.Drawing.Point(463, 329);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Player";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(225, 406);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 32);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Rock";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(347, 406);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 32);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Paber";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(467, 406);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 32);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Lõikerist";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label2.Location = new System.Drawing.Point(463, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "AI";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label9.Location = new System.Drawing.Point(328, 201);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(147, 25);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Võita/Kaotada";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label4.Location = new System.Drawing.Point(515, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 24);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label5.Location = new System.Drawing.Point(13, 406);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 24);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Ümar";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label6.Location = new System.Drawing.Point(89, 406);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 24);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "0";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::rpsgame2.Properties.Resources.backform1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rpsgame2
+{
+    public partial class Form2 : Form
+    {
+        public int rounds = 3;
+        public int timePerRound = 6;
+        string[] AIchoice = { "rock", "paper", "scissor", "rock", "scissor", "paper" };
+        public int randomNumber;
+        string command;
+        Random rnd = new Random();
+        string playerChoice;
+        int playerWins = 0;
+        int AIwins = 0;
+        public Form2()
+        {
+            InitializeComponent();
+            playerChoice = "none";
+        }
+
+        private void Btn_rock_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label4.Text = Convert.ToString(timePerRound);
+            if (timePerRound < 1)
+            {
+
+                timer1.Enabled = false;
+
+                timePerRound = 6;
+                randomNumber = rnd.Next(0, 5);
+                command = AIchoice[randomNumber];
+
+
+
+                switch (command)
+
+                {
+
+                    case "rock":
+                        pictureBox2.Image = Properties.Resources.rock;
+                        break;
+
+                    case "paper":
+                        pictureBox2.Image = Properties.Resources.paper;
+                        break;
+
+                    case "scissor":
+                        pictureBox2.Image = Properties.Resources.scissors;
+                        break;
+
+                    default:
+                        break;
+
+                }
+
+
+                if (rounds > 1)
+                {
+                    checkGame();
+                }
+                else
+                {
+                    decisionEngine();
+                }
+            }
+        }
+        private void checkGame()
+        {
+            if (playerChoice == "rock" && command == "paper")
+            {
+                MessageBox.Show("AI Wins");
+                AIwins++;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "paper" && command == "rock")
+            {
+                MessageBox.Show("player Wins");
+                playerWins++;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "paper" && command == "scissor")
+            {
+                MessageBox.Show("AI Wins");
+                AIwins++;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "scissor" && command == "paper")
+            {
+                MessageBox.Show("player Wins");
+                playerWins++;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "scissor" && command == "rock")
+            {
+                MessageBox.Show("AI Wins");
+                AIwins++;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "rock" && command == "scissor")
+            {
+                MessageBox.Show("player Wins");
+                playerWins++;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "none")
+            {
+                MessageBox.Show(label1.Text + " Make a choice");
+                nextRound();
+            }
+            else
+            {
+                MessageBox.Show("Draw");
+                nextRound();
+            }
+        }
+        private void decisionEngine()
+        {
+            if (playerWins > AIwins)
+            {
+                label9.Text = label1.Text + " Wins the game";
+            }
+            else
+            {
+                label9.Text = "AI Wins the game";
+            }
+        }
+        private void nextRound()
+        {
+            playerChoice = "none";
+            pictureBox1.Image = Properties.Resources.quest;
+            timer1.Enabled = true;
+            pictureBox2.Image = Properties.Resources.quest;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            playerChoice = "rock";
+            pictureBox1.Image = Properties.Resources.rock;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            playerChoice = "paper";
+            pictureBox1.Image = Properties.Resources.paper;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            playerChoice = "scissor";
+            pictureBox1.Image = Properties.Resources.scissors;
+        }
+    }
+}
